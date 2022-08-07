7bbda978d01e97c707ddae0adae884e52343a4f9
Ayush-dixit-15 <105589099+Ayush-dixit-15@users.noreply.github.com>
2022-08-07 11:37:28 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import Logo from '../images/Logo-1.png'
+import Logo from '../images/Logo.png'
 
 const Footer = () => {
   return (
     <Box mt="80px" bgColor="#fff3f4">
        <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-        <img src={Logo} alt="Logo" wodth="200px" height="40px"/>
+       <img src = {Logo} alt = "logo" style = {{width:'48px', height: '48px', margin: '0 20px'}} />
     
        <Typography variant ="h5" pb="40px" mt="20px">
         Made under the Guidance of J.S.M
