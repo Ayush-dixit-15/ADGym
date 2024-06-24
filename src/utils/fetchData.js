51798de4a6a89ac8ba0d5ac21cd35a208c6173f3
Ayush-dixit-15 <105589099+Ayush-dixit-15@users.noreply.github.com>
2024-06-24 14:02:17 +0530

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,15 +2,15 @@ export const exerciseOptions = {
     method: 'GET',
     url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
-      'X-RapidAPI-Key': 'c568d93ccfmshc057dc6bf7c9136p187f35jsn1ee2c54ee5d8',
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    } 
+    'x-rapidapi-key': '9e086ca345msh0d8f3e16fdf6311p1d4168jsn6cd237c5ad9d',
+    'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+  }
   };
 
 export const ytoptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'c568d93ccfmshc057dc6bf7c9136p187f35jsn1ee2c54ee5d8',
+    'X-RapidAPI-Key': '9e086ca345msh0d8f3e16fdf6311p1d4168jsn6cd237c5ad9d',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
